b71b17806c2b74e2f4558d7d3b5e90e37086fc3f
omedcodes <omedsafi21@gmail.com>
2026-03-08 23:39:18 +0100

    Refactor: remove repeating M_PI declerations and fix cylinder formula

--- src/llml.h
+++ src/llml.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846f
+#endif
+
 #include "area/circle.h"
 #include "area/square.h"
 #include "area/rectangle.h"
--- src/volume/sphere.h
+++ src/volume/sphere.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#ifndef M_PI
-    #define M_PI 3.14159265358979323846
-#endif
-
 static inline float volume_sphere(float radius) 
 {
     return 0.75f * M_PI * (radius * radius * radius);
@@ -11,5 +7,5 @@ static inline float volume_sphere(float radius)
 
 static inline float surface_area_sphere(float radius)
 {
-    return 4 * M_PI * (radius * radius);
+    return (4.0f / 3.0f) * M_PI * (radius * radius * radius);
 }
