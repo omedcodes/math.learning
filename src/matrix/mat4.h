d42e6000450bbf13d040a301f5caaa9a08c76db6
omedcodes <omedsafi21@gmail.com>
2026-03-09 17:32:06 +0100

    implement perspective matrix and camera matrix with look at function

--- src/matrix/mat4.h
+++ src/matrix/mat4.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../vector/vec3.h"
+#include <math.h>
 
 typedef struct {
     float data[16];
@@ -46,5 +47,47 @@ static inline vec3 mat4_mul_vec3(mat4 m, vec3 v) {
     result.x = m.data[0] * v.x + m.data[4] * v.y + m.data[8] * v.z + m.data[12];
     result.y = m.data[1] * v.x + m.data[5] * v.y + m.data[9] * v.z + m.data[13];
     result.z = m.data[2] * v.x + m.data[6] * v.y + m.data[10] * v.z + m.data[14];
+    return result;
+}
+
+static inline mat4 mat4_perspective(float fov_radians, float aspect, float near_p, float far_p) {
+    mat4 result = {0};
+    float tan_half_fov = tanf(fov_radians / 2.0f);
+    result.data[0] = 1.0f / (aspect * tan_half_fov);
+    result.data[5] = 1.0f / tan_half_fov;
+    result.data[10] = far_p / (near_p - far_p);
+    result.data[11] = -1.0f;
+    result.data[14] = -(far_p * near_p) / (far_p - near_p);
+    return result;
+}
+
+static inline mat4 mat4_ortho(float left, float right, float bottom, float top, float near_p, float far_p) {
+    mat4 result = mat4_identity();
+    result.data[0] = 2.0f / (right - left);
+    result.data[5] = 2.0f / (top - bottom);
+    result.data[10] = -1.0f / (far_p - near_p);
+    result.data[12] = -(right + left) / (right - left);
+    result.data[13] = -(top + bottom) / (top - bottom);
+    result.data[14] = -near_p / (far_p - near_p);
+    return result;
+}
+
+static inline mat4 mat_look_at(vec3 eye, vec3 target, vec3 up) {
+    vec3 f = vec3_normalized(vec3_subtract(target, eye));
+    vec3 s = vec3_normalized(vec3_cross(f, up));
+    vec3 u = vec3_cross(s, f);
+    mat4 result = mat4_identity();
+    result.data[0] = s.x;
+    result.data[4] = s.y;
+    result.data[8] = s.z;
+    result.data[1] = u.x;
+    result.data[5] = u.y;
+    result.data[9] = u.z;
+    result.data[2] = -f.x;
+    result.data[6] = -f.y;
+    result.data[10] = -f.z;
+    result.data[12] = -vec3_dot(s, eye);
+    result.data[13] = -vec3_dot(u, eye);
+    result.data[14] = vec3_dot(f, eye);
     return result;
 }
