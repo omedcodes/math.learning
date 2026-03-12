b48bc82a626c72bfb0e23a75c6bb50e23fbdd3b9
omedcodes <omedsafi21@gmail.com>
2026-03-12 20:40:04 +0100

    add vec2 and vec4 full set utilities

--- src/vector/vec2.h
+++ src/vector/vec2.h
@@ -1,9 +1,7 @@
 #pragma once
-
 #include <math.h>
 
-typedef struct
-{
+typedef struct {
     float x;
     float y;
 } vec2;
@@ -11,5 +9,13 @@ typedef struct
 static inline vec2 vec2_create(float x, float y) { return (vec2){x, y}; }
 
 static inline vec2 vec2_add(vec2 a, vec2 b) { return (vec2){a.x + b.x, a.y + b.y}; }
+static inline vec2 vec2_subtract(vec2 a, vec2 b) { return (vec2){a.x - b.x, a.y - b.y}; }
+static inline vec2 vec2_scale(vec2 v, float scalar) { return (vec2){v.x * scalar, v.y * scalar}; }
 
+static inline float vec2_dot(vec2 a, vec2 b) { return (a.x * b.x) + (a.y * b.y); }
 static inline float vec2_length(vec2 v) { return sqrtf(v.x * v.x + v.y * v.y); }
+
+static inline vec2 vec2_normalized(vec2 v) {
+    float len = vec2_length(v);
+    return (len > 0.0f) ? vec2_scale(v, 1.0f / len) : (vec2){0.0f, 0.0f};
+}
--- src/vector/vec4.h
+++ src/vector/vec4.h
@@ -1,19 +1,23 @@
 #pragma once
-
+#include <math.h>
 #include "vec3.h"
 
-typedef struct
-{
-    float x;
-    float y;
-    float z;
-    float w;
+typedef struct {
+    float x, y, z, w;
 } vec4;
 
-static inline vec4 vec4_create(float x, float y, float z, float w) {
-    return (vec4){x, y, z, w};
-}
+static inline vec4 vec4_create(float x, float y, float z, float w) { return (vec4){x, y, z, w}; }
 
-static inline vec4 vec4_from_vec3(vec3 v, float w) {
-    return (vec4){v.x, v.y, v.z, w};
+static inline vec4 vec4_from_vec3(vec3 v, float w) { return (vec4){v.x, v.y, v.z, w}; }
+
+static inline vec4 vec4_add(vec4 a, vec4 b) { return (vec4){a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w}; }
+static inline vec4 vec4_subtract(vec4 a, vec4 b) { return (vec4){a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w}; }
+static inline vec4 vec4_scale(vec4 v, float scalar) { return (vec4){v.x * scalar, v.y * scalar, v.z * scalar, v.w * scalar}; }
+
+static inline float vec4_dot(vec4 a, vec4 b) { return (a.x * b.x) + (a.y * b.y) + (a.z * b.z) + (a.w * b.w); }
+static inline float vec4_length(vec4 v) { return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w); }
+
+static inline vec4 vec4_normalized(vec4 v) {
+    float len = vec4_length(v);
+    return (len > 0.0f) ? vec4_scale(v, 1.0f / len) : (vec4){0.0f, 0.0f, 0.0f, 0.0f};
 }
